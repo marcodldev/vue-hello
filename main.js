8d7db97489de2ea6e7ffdaae0e004c83afc0cc2f
marcodldev <marcodelisi2@gmail.com>
2022-11-10 17:08:39 +0100

    + vue class

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var app = new Vue(
       data: {
        message: 'Oasis Knebworth 1996 live photo',
        img: './oasis-1996-c.jpg',
+       imgclasse: 'border border-5',
+       h1classe: 'text-center text-yellow p-4',
+       innrootdivclasse: 'container bg-black text-light',
      }
     }
  );
-
- vfor
