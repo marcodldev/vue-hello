0d3fad77bc2c7bb37453ca372e1258ddf45d8b5e
marcodldev <marcodelisi2@gmail.com>
2022-11-10 16:58:53 +0100

    min fix

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var app = new Vue(
       el: '#root',
       data: {
        message: 'Oasis Knebworth 1996 live photo',
-       img: '/oasis-1996-c.jpg',
+       img: './oasis-1996-c.jpg',
      }
     }
  );
+
+ vfor
